f552bbae0cb3cd572cdbe8285cfb8c63fd21883f
Pretamaquiada <129337868+Pretamaquiada@users.noreply.github.com>
2023-04-05 21:28:24 -0300

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -3,27 +3,21 @@
   
   "use strict";
 
-    // COUNTER NUMBERS
-    jQuery('.counter-thumb').appear(function() {
-      jQuery('.counter-number').countTo();
+    // MENU
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
     });
-
-    // BACKSTRETCH SLIDESHOW
-    $('.hero-section').backstretch([
-      "images/slideshow/afro-woman-cleaning-window-with-rag-home.jpg", 
-      "images/slideshow/afro-woman-holding-bucket-with-cleaning-items.jpg",
-      "images/slideshow/unrecognizable-cleaner-walking-into-hotel-room-with-tools-detergents.jpg"
-    ],  {duration: 2000, fade: 750});
     
     // CUSTOM LINK
     $('.smoothscroll').click(function(){
       var el = $(this).attr('href');
       var elWrapped = $(el);
+      var header_height = $('.navbar').height();
   
-      scrollToDiv(elWrapped);
+      scrollToDiv(elWrapped,header_height);
       return false;
   
-      function scrollToDiv(element){
+      function scrollToDiv(element,navheight){
         var offset = element.offset();
         var offsetTop = offset.top;
         var totalScroll = offsetTop-navheight;
@@ -33,7 +27,7 @@
         }, 300);
       }
     });
-    
+  
   })(window.jQuery);
 
 
